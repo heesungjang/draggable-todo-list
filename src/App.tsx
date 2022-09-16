d0b7edccff073e22eba03762ac32608322ccf114
heesungjang <heesungj7@gmail.com>
2022-09-16 16:15:34 +1000

    스타일 컴포넌트 init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  AppContainer,
+  ColumnContainer,
+  ColumnTitle,
+  CardContainer,
+} from './styles';
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <ColumnContainer>
+        <ColumnTitle>Todo:</ColumnTitle>
+        <CardContainer>FirstItem</CardContainer>
+        <CardContainer>SecondItem</CardContainer>
+        <CardContainer>ThirdItem</CardContainer>
+      </ColumnContainer>
+    </AppContainer>
   );
-}
-
-export default App;
+};
